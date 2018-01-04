33deea64cb9b21417a91132110be4af2afd6eb1b
njang <neo.w.jang@gmail.com>
2018-01-04 15:17:48 -0600

    Afternoon lesson on REST with express

--- app.js
+++ app.js
@@ -1,6 +1,44 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello World!'))
+// app.get('/', (req, res) => res.send('Hello World!'))
+// app.get('/kittens', (req, res) => res.send('Hello Kittens!'))
+// app.get('', (req, res) => res.send(input))
 
-app.listen(9001, () => console.log('Example app listening on port 9001!'))
+// app.listen(3000, () => console.log('Example app listening on port 3000!'))
+
+
+const webPages = [
+  {
+    url: "/kittens",
+    name: "Picture of Kittens",
+    imageLink: "https://metrouk2.files.wordpress.com/2017/07/187144066.jpg?w=748&h=498&crop=1"
+  },
+  {
+    url: "/puppies",
+    name: "Picture of Puppies",
+    imageLink: "http://www.justpuppies.net/images/pom_triple_0815.jpg"
+  },
+  {
+    url: "/ferrets",
+    name: "Picture of Ferrets",
+    imageLink: "https://listverse.com/wp-content/uploads/2013/12/colours.jpg"
+  }
+];
+
+let mainPageLinks = "";
+
+// for (let i = 0; i < webPages.length; i++) {
+//   mainPageLinks = mainPageLinks + "<a href=" + webPages[i].url + ">" + webPages[i].name + "</a><br>";
+// }
+
+app.get('/', (req, res) => {
+  res.send(mainPageLinks);
+})
+
+for (let i = 0; i < webPages.length; i++) {
+  app.get(webPages[i].url, (req, res) => res.send('<img src=' + webPages[i].imageLink + '><br>' + mainPageLinks));
+}
+
+
+app.listen(3000, () => console.log('Kittens!'));
--- server.js
+++ server.js
@@ -1,11 +1,67 @@
 const express = require('express'); //from documentation: express is function
 const app = express();//app is an object
 
-app.get('/somedata', (request, response) => {
-    response.send('here is your information');
-    console.log(request);
+app.get('', (request, response) => {
+    response.send('Let\'s start');
 });
 
-app.listen(3000, ()=>{
+// GET ALL RESTAURANTS "/restaurants"
+// GET RESTAURANTS BY ID "/restaurants/:id"
+// GET ALL CUISINES "/cuisines"
+// GET CUISINE: "/cuisines/:id"
+// GET ALL COST RANGES: "/costs"
+// GET COST RANGE: "/costs:id"
+// GET ALL RESTAURANTS BY CUISINE "/cuisines/:id/restaurants"
+// GET RESTAURANTS BY CUISINE "/cuisines/:id/restaurants/:id"
+// GET ALL RESTAURANTS BY CUISINE BY COST RANGE "/cuisines/:id/costs/:id/resturants"
+
+const rest = [
+	// {
+ //    url: "/restaurants",
+ //    name: "Get all resturants"
+ //  },
+  {
+    url: "/restaurants/:id",
+    name: "Get a resturant"
+  },
+	{
+    url: "/cuisines",
+    name: "Get all cuisines"
+  },
+	{
+    url: "/cuisines/:id",
+    name: "Get a cuisine"
+  },
+	{
+    url: "/costs",
+    name: "Get all cost ranges"
+  },
+	{
+    url: "/costs/:id",
+    name: "Get a cost range"
+  }
+]
+
+// for (let i = 0; i < rest.length; i++) {
+//   app.get(rest[i].url, (request, response) => {response.send(rest[i].name)});
+// }
+
+const restaurants = [
+  {id: 1, name: "Vaquero Taquero", cuisine: "Mexican", url: "http://www.vaquerotaquero.com/"},
+  {id: 2, name: "Manna Resaurant", cuisine: "Korean", url: "https://ibigmedia1.wixsite.com/mannaaustin/menu"},
+  {id: 3, name: "Franklin Street BBQ", cuisine: "BBQ", url: "https://franklinbbq.com/"},
+  {id: 4, name: "Chilantro", cuisine: "Fusion", url: "http://www.chilantrobbq.com/"}
+];
+
+app.get('/restaurants', (request, response) => {
+  let restaurantsJSON = JSON.stringify(restaurants)
+  response.send(restaurantsJSON);
+});
+
+for (let i = 0; i < restaurants.length; i++) {
+  app.get('/restaurants/' + (i + 1), (request, response) => {response.send('<a href="' + restaurants[i].url + '">' + restaurants[i].name + '</a>')});
+}
+
+app.listen(3000, () => {
     console.log("I am listening");
 });
